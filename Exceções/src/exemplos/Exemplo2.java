5edabd9e602bf807e04be7c9adf15fe10b23edc8
Aluno <Aluno@10.10.56.8>
2017-02-02 21:35:44 -0300

    MEnsagem modificada

--- Exceções/src/exemplos/Exemplo2.java
+++ Exceções/src/exemplos/Exemplo2.java
@@ -22,18 +22,11 @@ public class Exemplo2 {
 		System.out.println("inicio do metodo2");
 		int[] array = new int[10];
 		for (int i = 0; i <= 15; i++) {
-			
-			
-			
-			
-			
 				array[i] = i;
-			
-				// TODO Auto-generated catch block
-				
-		
+
 			System.out.println(i);
 		}
 		System.out.println("fim do metodo2");
+		System.out.println("eae minha galerinha");
 	}
 }
